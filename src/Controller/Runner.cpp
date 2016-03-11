4d972d68eb6d3ef1ee0d1cb6a28a9ea31380720e
denonbarton <dbar0540@740s-pl-126.local>
2016-03-11 13:55:03 -0700

    1123

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,25 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: dbar0540
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is how you talk to the console in C++" << endl ;
+	//This is like System.out.println("Some text"); in Java.
+
+
+	int temp;
+
+	cin>> temp;
+
+	cout << temp;
+
+	return 0;
+}
+
+
